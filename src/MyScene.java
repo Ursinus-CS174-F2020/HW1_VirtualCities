1c59108e0d70349c25d52c1dc201b22fe52a2677
Chris Tralie <chris.tralie@gmail.com>
2020-09-11 20:03:53 -0400

    Finished starter code

--- src/MyScene.java
+++ src/MyScene.java
@@ -1,28 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author ctralie
+ * A class that holds methods to draw different 3D objects and piece
+ * together scenes
  */
 public class MyScene {
     
-    
-    public static void main(String[] args) {
+    /**
+     * Draw a city block repeated several times
+     */
+    public static void drawScene() {
         Scene3D scene = new Scene3D();
         scene.addCamera(0, 2, 0, 0);
+        scene.addCamera(0, 2, -4, 180);
         scene.addLight(0, 100, 0, 1, 1, 1);
         scene.addLight(0, -100, 0, 1, 1, 1);
         scene.addLight(-100, 100, 0, 1, 1, 1);
         scene.addLight(100, -100, 0, 1, 1, 1);
         
         // Add a large gray box for the ground
-        scene.addBox(0, -25, 0, 1000, 50, 1000, 0.5, 0.5, 0.5);
-        // Draw a cyan sphere with radius 1 centered at (-4, 4, 0)
-        scene.addSphere(-4, 4, 0, 1, 0, 1, 1);
-        scene.saveScene("myscene.json", "Chris's Scene");
+        scene.addBox(0, -25, 0, 1000, 50, 1000, 127, 127, 127);
+        
+        // TODO: Fill this in
+        
+        scene.saveScene("myscene.json", "Scene Title");
+    }
+    
+    /**
+     * Draw your art contest submission
+     */
+    public static void drawArtContest() {
+        Scene3D scene = new Scene3D();
+        scene.addCamera(0, 2, 0, 0);
+        // TODO: Fill this in
+        
+        scene.saveScene("artcontest.json", "Scene Title");
+    }
+    
+    public static void main(String[] args) {
+        drawScene();
+        drawArtContest();
     }
 }
